d4c746484665fa4f7ec54c4255816d71bae6a607
retroam <retroam@yahoo.com>
2014-03-14 11:23:29 -0400

    changes to plotting of receptor variable

--- receptorCompute.m
+++ receptorCompute.m
@@ -28,10 +28,10 @@ params = receptorPARAMS(KR,KL,KA,KG,alpha_L,alpha_A,gamma_L,gamma_A);
 load KLcalc; load fitalpha;
 KL = KLcalc(1);
 alpha_L = fitalpha(1);
-KA = KLcalc(18);
-alpha_A = fitalpha(18);
+KA = KLcalc(17);
+alpha_A = fitalpha(17);
 KG = 2.4131;gamma_L =  0.3762;
-p = receptorPARAMS(KR,KL,KA,KG,alpha_L,alpha_A,gamma_L,gamma_A);
+p = receptorPARAMS(KR,KL,1e1*KA,KG,alpha_L,alpha_A,gamma_L,gamma_A);
 
 y0 = zeros(2,1);[~,y] = ode15s(@receptorODE,[0; 20*60*1000],y0,options,p);
 y0 = y(end,:);
@@ -43,20 +43,20 @@ for tstep=1:length(t1),
 end
 
 tspan = [2*60*1000; 6*60*1000]; y0 = y1(end,:);
-p(1) = 0.1;p(2) = 0;
+p(1) = 0.1;p(2) = 0.3;
 [t2,y2] = ode15s(@receptorODE,tspan,y0,options,p);
 for tstep=1:length(t2),
     [~,algvars2(tstep,:)]=receptorODE(t2(tstep),y2(tstep,:),p);
 end
 
 tspan = [6*60*1000; 10*60*1000]; y0 = y2(end,:);
-p(1) = 10;p(2) = 0;
+p(1) = 10;p(2) = 0.3;
 [t3,y3] = ode15s(@receptorODE,tspan,y0,options,p);
 for tstep=1:length(t3),
     [~,algvars3(tstep,:)]=receptorODE(t3(tstep),y3(tstep,:),p);
 end
 
-tGly = [t1;t2;t3;]./60;
+tGly = [t1;t2;t3;]./60e3;
  yGly = [y1;y2;y3;];
 algvarsGly = [algvars1;algvars2;algvars3;];
 figure(1);
@@ -65,16 +65,21 @@ yCell=mat2cell(yGly,size(yGly,1),ones(size(yGly,2),1));
 %%
 [Ri,G] = yCell{:};
  Rtot = sum(algvarsGly,2) + Ri ;
- %algvarsGly =  100*algvarsGly./repmat(Rtot,1,8);
+% algvarsGly =  100*algvarsGly./repmat(Rtot,1,8);
  idx = Rtot< .0132+1e-4;
  algvarsCell=mat2cell(algvarsGly,size(algvarsGly,1),ones(size(algvarsGly,2),1));
 [Ra, LRi ,LRa, RaG, LRaG ,ARi, ARa ,ARaG] =  algvarsCell{:};
 %%
 color2= [0.6 0 0];color1 = [0.5 0.5 0.5];
- subplot(2,3,1);plot(tGly(idx),Ra(idx),'LineWidth',2,'Color',color1);ylabel('Ra (% \beta1-AR)');hold all;xlabel('time (min)');
- subplot(2,3,2);plot(tGly(idx),LRa(idx),'LineWidth',2,'Color',color1);ylabel('LRa (% \beta1-AR)');hold all;xlabel('time (min)');
-subplot(2,3,3);plot(tGly(idx),RaG(idx),'LineWidth',2,'Color',color1);ylabel('RaG (% \beta1-AR)');hold all;xlabel('time (min)');
-subplot(2,3,4);plot(tGly(idx),LRaG(idx),'LineWidth',2,'Color',color1);ylabel('LRaG (% \beta1-AR)');hold all;xlabel('time (min)');
-subplot(2,3,5);plot(tGly(idx),ARi(idx),'LineWidth',2,'Color',color1);ylabel('ARi (% \beta1-AR)');hold all;xlabel('time (min)');
-subplot(2,3,6);plot(tGly(idx),Rtot(idx),'LineWidth',2,'Color',color1);xlabel('time (min)');ylabel('Rtot');hold all;
-
+r = 0.0132;
+ subplot(2,3,1);plot(tGly(idx),Ra(idx)./r,'LineWidth',2,'Color',color1);ylabel('Ra (% \beta1-AR)');hold all;xlabel('time (min)');
+ subplot(2,3,2);plot(tGly(idx),LRa(idx)./r,'LineWidth',2,'Color',color1);ylabel('LRa (% \beta1-AR)');hold all;xlabel('time (min)');
+subplot(2,3,3);plot(tGly(idx),RaG(idx)./r,'LineWidth',2,'Color',color1);ylabel('RaG (% \beta1-AR)');hold all;xlabel('time (min)');
+subplot(2,3,4);plot(tGly(idx),LRaG(idx)./r,'LineWidth',2,'Color',color1);ylabel('LRaG (% \beta1-AR)');hold all;xlabel('time (min)');
+subplot(2,3,5);plot(tGly(idx),ARi(idx)./r,'LineWidth',2,'Color',color1);ylabel('ARi (% \beta1-AR)');hold all;xlabel('time (min)');
+subplot(2,3,6);plot(tGly(idx),Rtot(idx),'LineWidth',2,'Color',color1);xlabel('time (min)');ylabel('Rtot (\muM)');hold all;
+%%
+set(gcf, 'PaperPositionMode', 'manual');
+ set(gcf, 'PaperUnits', 'inches');
+ set(gcf, 'PaperPosition', [0.0 3.5 9.0  6.2]);
+print -dpdf KA_changes
