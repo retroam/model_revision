1e11efdfde6a633f138227945c3d93ab498919f9
retroam <rka2p@ALD423-501.eservices.virginia.edu>
2014-03-17 16:17:19 -0400

    Changes to BBdose_plot

--- BBdosewflag.m
+++ BBdosewflag.m
@@ -1,4 +1,4 @@
- cd('B:\Robert\Dissertation\Beta Blocker modeling\final_model validation');
+ 
 close all; clear all;clc;
 KL = 0.2;
 KR =  10;%10 [uM] 
@@ -10,7 +10,7 @@ PKAItot         = 0.59;           % (uM) total type 1 PKA
 PKAIItot        = 0.025;          % (uM) total type 2 PKA
 
 
-load fitalpha.mat;load Ki;
+load fitalpha.mat;
  drugList = {'Isoproterenol'; 'Basal' ;'Epinephrine'; 'Norepinephrine'; 'Fenoterol' ;'Formoterol';
 'Salbutamol' ;'Terbutaline' ;'Broxaterol' ;'Salmeterol'; 'BRL-37344'; 'CGP-12177';
 'Alprenolol' ;'Pindolol'; 'SR 59230A'; 'Atenolol' ;'Carvedilol' ;'Metoprolol'; 'Bisoprolol';
@@ -22,17 +22,20 @@ load fitalpha.mat;load Ki;
   KLcalc =Ki.*(fitalpha*KR + 1)./(fitalpha*(KR + 1));
    KLcalc = (0.2/KLcalc(1))*KLcalc; 
 KG = 2.4131;gamma_L =  0.3762;
+flag = 0;
+scaling_factor = 0.1;
 %%
-p = daePARAMS(KR,KL,KA,KG,alpha_L,alpha_A,gamma_L,gamma_A);
+p = daePARAMS(KR,KL,KA,KG,alpha_L,alpha_A,gamma_L,gamma_A,scaling_factor);
 
 RelTol = 1e-13;MaxStep = 1e-1;
 options = odeset('MaxStep',1e3,'NonNegative',[1:29],'RelTol',RelTol);
  y0 = zeros(29,1); y0(1) = 0.0132;y0(11) = PKAItot ; y0(17) = PKAIItot;
-t = [0;1000*60*1000]; p(1:2) = 0;[~,y] = ode15s(@daeODE,t,y0,options,p);y0 = y(end,:);
+t = [0;1000*60*1000]; p(1:2) = 0;[~,y] = ode15s(@daeODE,t,y0,options,p,flag);y0 = y(end,:);
 tspan = [0;2*60*1000]; 
 dose = 10.^[-5:.1:3];
 
  beta_blocker = [20 18 17];
+ flag = [0 0 1];
 % beta_blocker = [1:22];
 for i=1:length(beta_blocker)
     for j = 1:length(dose)
@@ -40,20 +43,20 @@ for i=1:length(beta_blocker)
         KL =  KLcalc(1);
         alpha_L = fitalpha(1);
         KA = KLcalc(beta_blocker(i)); alpha_A = fitalpha(beta_blocker(i));
-        p = daePARAMS(KR,KL,KA,KG,alpha_L,alpha_A,gamma_L,gamma_A);
+        p = daePARAMS(KR,KL,KA,KG,alpha_L,alpha_A,gamma_L,gamma_A,scaling_factor);
         tspan = [0;2*60*1000];
         p(1) = 0; p(2) = 0;  % agonist conc and beta blocker conc
-        [t1,y] = ode15s(@daeODE,tspan,y0,options,p);
+        [t1,y] = ode15s(@daeODE,tspan,y0,options,p,flag(1));
         bg(j,1) = max(y(end,9));
         yfinal = y(end,:); 
         tspan = [2*60*1000;6*60*1000];
         p(1) = 0.1; p(2) = dose(j);  % agonist conc and beta blocker conc
-        [t2,y2] = ode15s(@daeODE,tspan,yfinal,options,p); 
+        [t2,y2] = ode15s(@daeODE,tspan,yfinal,options,p,flag(i)); 
         bg(j,2) = max(y2(end,9));
         yfinal = y2(end,:); 
         tspan2 = [6*60*1000 10*60*1000]; %changed the time
         p(1) = 10; p(2) = dose(j);     % agonist conc and beta blocker conc
-        [t3,y3]=ode15s(@daeODE,tspan2,yfinal,options,p);
+        [t3,y3]=ode15s(@daeODE,tspan2,yfinal,options,p,flag(i));
         bg(j,3) = max(y3(end,9));
     end 
          sensitivity(:,i) = (bg(:,3)- bg(:,2));
@@ -71,7 +74,7 @@ set(gcf, 'PaperPositionMode', 'manual');
  set(gcf, 'PaperUnits', 'inches');
  set(gcf, 'PaperPosition', [2.9 4.4 2.8 2.3]);
 cd('A:\Robert\Manuscripts\Modeling Beta1-adrenergic receptor blockers and polymorphisms in cardiac myocytes\Figures\Reviewers');
-print -dpdf BBdoseA
+print -dpdf BBdoseAwflag
 %%
 close all
 figure;subplot(1,3,1);semilogx(dose*1e-6,[sensitivity(:,1) response(:,1)],'Linewidth',2);hold all;
@@ -91,7 +94,7 @@ line('XData',[1e-6 1e-6],'YData',[-1 4],'LineStyle','-.','LineWidth',2);
  set(gcf, 'PaperUnits', 'inches');
  set(gcf, 'PaperPosition', [0.5 4.4 7.4 2.3]);
 cd('A:\Robert\Manuscripts\Modeling Beta1-adrenergic receptor blockers and polymorphisms in cardiac myocytes\Figures\Reviewers');
-print -dpdf BBdoseB
+print -dpdf BBdoseBwflag
 
 %% carvedilol with change in alpha
 
@@ -202,48 +205,46 @@ subplot(1,3,3);errorbar(CAR(:,1),CAR(:,2),CAR(:,3),'o','color','g');
 
 %% carvedilol new dose
 
-% run('B:\Robert\Dissertation\Beta Blocker modeling\final_model validation\data\CARsensexp');close all;
-% 
-% cd('B:\Robert\Dissertation\Beta Blocker modeling\final_model validation');
-%  p = daePARAMS(KR,KL,KA,KG,alpha_L,alpha_A,gamma_L,gamma_A);p(1) = 0;
-%  y0 = zeros(29,1);y0(1) = 0.0132;y0(11) = PKAItot ; y0(17) = PKAIItot;
-%  [~,y] = ode15s(@daeODE,[0;60*60*1000],y0,[],p);y0Sig = y(end,:);
-% 
-% 
-% options = odeset('RelTol',1e-5,'MaxStep',5e-3,'Stats','on');
-% p = daePARAMSc(KR,KL,KA,KG,alpha_L,alpha_A,gamma_L,gamma_A);
-% load y0full;y0 = y0full;
-% dose = 0.3;drugs = 17; 
-% KL = KLcalc(1);
-% alpha_L = fitalpha(1);
-% KA = KLcalc(drugs); alpha_A = fitalpha(drugs);
-% p = daePARAMSc(KR,KL,KA,KG,alpha_L,alpha_A,gamma_L,gamma_A);
-%     
-% tspan = [0;2*60];
-% p(1) = 0; p(2) = 0;  % agonist conc and beta blocker conc
-% [t,y] = ode15s(@daeODEc,tspan,y0,options,p);
-% 
-% bgcAMP{1,1} = y(:,9);
-% bgCa{1,1} = y(:,47);
-% time{1,1} = t;
-% yfinal = y(end,:); 
-% 
-% tspan = [2*60;6*60];
-% p(1) = 0.1; p(2) = dose;  % agonist conc and beta blocker conc
-% [t2,y2] = ode15s(@daeODEc,tspan,yfinal,options,p); 
-% bgcAMP{1,2} = y2(:,9);
-% bgCa{1,2} = y2(:,47);
-% time{1,2} = t2;
-% yfinal = y2(end,:); 
-% tspan2 = [6*60 10*60]; %changed the time
-% p(1) = 10; p(2) = dose;     % agonist conc and beta blocker conc
-% [t3,y3]=ode15s(@daeODEc,tspan2,yfinal,options,p);
-% bgcAMP{1,3} = y3(:,9);
-% bgCa{1,3} = y3(:,47);
-% time{1,3} = t3;
-% 
-% 
-% CARmodel = [bgCa{1,1}; bgCa{1,2} ;bgCa{1,3}];CARmodelt = [time{1,1}; time{1,2} ;time{1,3}]./60;
+%run('B:\Robert\Dissertation\Beta Blocker modeling\final_model validation\data\CARsensexp');close all;
+
+
+ p = daePARAMS(KR,KL,KA,KG,alpha_L,alpha_A,gamma_L,gamma_A,scaling_factor);p(1) = 0;
+ y0 = zeros(29,1);y0(1) = 0.0132;y0(11) = PKAItot ; y0(17) = PKAIItot;flag = 0;
+ [~,y] = ode15s(@daeODE,[0;60*60*1000],y0,[],p,flag);y0Sig = y(end,:);
+options = odeset('RelTol',1e-5,'MaxStep',5e-3,'Stats','on');
+
+load y0full;y0 = y0full;
+dose = 0.3;drugs = 17; 
+KL = KLcalc(1);
+alpha_L = fitalpha(1);
+KA = KLcalc(drugs); alpha_A = fitalpha(drugs);
+p = daePARAMSc(KR,KL,KA,KG,alpha_L,alpha_A,gamma_L,gamma_A,scaling_factor);
+    
+tspan = [0;2*60];
+p(1) = 0; p(2) = 0; flag = 0; % agonist conc and beta blocker conc
+[t,y] = ode15s(@daeODEc,tspan,y0,options,p,flag);
+
+bgcAMP{1,1} = y(:,9);
+bgCa{1,1} = y(:,47);
+time{1,1} = t;
+yfinal = y(end,:); 
+
+tspan = [2*60;6*60];
+p(1) = 0.1; p(2) = dose;flag = 1;  % agonist conc and beta blocker conc
+[t2,y2] = ode15s(@daeODEc,tspan,yfinal,options,p,flag); 
+bgcAMP{1,2} = y2(:,9);
+bgCa{1,2} = y2(:,47);
+time{1,2} = t2;
+yfinal = y2(end,:); 
+tspan2 = [6*60 10*60]; %changed the time
+p(1) = 10; p(2) = dose; flag = 1;    % agonist conc and beta blocker conc
+[t3,y3]=ode15s(@daeODEc,tspan2,yfinal,options,p,flag);
+bgcAMP{1,3} = y3(:,9);
+bgCa{1,3} = y3(:,47);
+time{1,3} = t3;
+
+
+CARmodel = [bgCa{1,1}; bgCa{1,2} ;bgCa{1,3}];CARmodelt = [time{1,1}; time{1,2} ;time{1,3}]./60;
 
 %%
 figure;subplot(2,1,1);plot(CARmodelt,CARmodel);box off;xlabel('time (min)');
